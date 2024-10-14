87d47fe0d710288b5973bd963535bf5f61614022
brunofbpaula <deepcooper@outlook.com>
2024-10-13 22:19:51 -0300

    feat: dtos

--- CP2.Domain/Interfaces/Dtos/IFornecedorDto.cs
+++ CP2.Domain/Interfaces/Dtos/IFornecedorDto.cs
@@ -2,6 +2,13 @@
 {
     public interface IFornecedorDto
     {
+        int Id { get; set; }
+        String Nome { get; set; }
+        String Cnpj { get; set; }
+        String Endereco { get; set; }
+        String Telefone { get; set; }
+        String Email { get; set; }
+        DateTime CriadoEm { get; set; }
 
         void Validate();
     }
--- CP2.Domain/Interfaces/Dtos/IVendedorDto.cs
+++ CP2.Domain/Interfaces/Dtos/IVendedorDto.cs
@@ -1,8 +1,19 @@
-﻿namespace CP2.Domain.Interfaces.Dtos
+﻿using System.Data;
+
+namespace CP2.Domain.Interfaces.Dtos
 {
     public interface IVendedorDto
     {
-
+        int Id { get; set; }
+        String Nome { get; set; }
+        String Email { get; set; }
+        String Telefone { get; set; }
+        DateTime DataNascimento { get; set; }
+        String Endereco { get; set; }
+        DataSetDateTime DataContratacao { get; set; }
+        Decimal ComissaoPercentual { get; set; }
+        Decimal MetaMensal { get; set; }
+        DateTime CriadoEm { get; set; }
         void Validate();
     }
 }
