c57c2aaf1bdde2f56cec276df8a0884281f67a67
brunofbpaula <deepcooper@outlook.com>
2024-10-13 21:05:35 -0300

    feat: models for 'fornecedor' and 'vendedor'

--- CP2.Domain/Entities/FornecedorEntity.cs
+++ CP2.Domain/Entities/FornecedorEntity.cs
@@ -2,9 +2,16 @@
 
 namespace CP2.Domain.Entities
 {
-    [Table("tb_")]
+    [Table("tb_fornecedor")]
     public class FornecedorEntity
     {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Cnpj { get; set; }
+        public String Endereco { get; set; }
+        public String Telefone { get; set; }
+        public String Email { get; set; }
+        public DateTime CriadoEm { get; set; }
 
     }
 }
