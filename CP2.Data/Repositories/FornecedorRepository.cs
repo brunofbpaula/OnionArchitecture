1379629cabc798aca2352664f522629340babb83
brunofbpaula <deepcooper@outlook.com>
2024-10-13 22:40:18 -0300

    feat: implement repositories' methods

--- CP2.Data/Repositories/FornecedorRepository.cs
+++ CP2.Data/Repositories/FornecedorRepository.cs
@@ -12,5 +12,69 @@ namespace CP2.Data.Repositories
         {
             _context = context;
         }
+
+        public IEnumerable<FornecedorEntity> ObterTodos()
+        {
+            var fornecedores = _context.Fornecedor.ToList();
+
+            return fornecedores;
+        }
+
+        public FornecedorEntity? ObterPorId(int id)
+        {
+            var fornecedor = _context.Fornecedor.Find(id);
+
+            if (fornecedor is not null)
+            {
+                return fornecedor;
+            }
+
+            return null;
+        }
+
+        public FornecedorEntity? SalvarDados(FornecedorEntity entity)
+        {
+            _context.Fornecedor.Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public FornecedorEntity? EditarDados(FornecedorEntity entity)
+        {
+            var fornecedor = _context.Fornecedor.Find(entity.Id);
+
+            if (fornecedor is not null)
+            {
+
+                fornecedor.Nome = entity.Nome;
+                fornecedor.Cnpj = entity.Cnpj;
+                fornecedor.Endereco = entity.Endereco;
+                fornecedor.Telefone = entity.Telefone;
+                fornecedor.Email = entity.Email;
+                fornecedor.CriadoEm = entity.CriadoEm;
+
+                _context.Fornecedor.Update(fornecedor);
+                _context.SaveChanges();
+
+                return fornecedor;
+            }
+
+            return null;
+        }
+        public FornecedorEntity? DeletarDados(int id)
+        {
+            var fornecedor = _context.Fornecedor.Find(id);
+
+            if (fornecedor is not null)
+            {
+                _context.Fornecedor.Remove(fornecedor);
+                _context.SaveChanges();
+
+                return fornecedor;
+            }
+
+            return null;
+        }
+
     }
 }
--- CP2.Data/Repositories/VendedorRepository.cs
+++ CP2.Data/Repositories/VendedorRepository.cs
@@ -12,5 +12,72 @@ namespace CP2.Data.Repositories
         {
             _context = context;
         }
+
+        public IEnumerable<VendedorEntity> ObterTodos()
+        {
+            var vendedores = _context.Vendedor.ToList();
+
+            return vendedores;
+        }
+
+        public VendedorEntity? ObterPorId(int id)
+        {
+            var vendedor = _context.Vendedor.Find(id);
+
+            if (vendedor is not null)
+            {
+                return vendedor;
+            }
+
+            return null;
+        }
+
+        public VendedorEntity? SalvarDados(VendedorEntity entity)
+        {
+            _context.Vendedor.Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public VendedorEntity? EditarDados(VendedorEntity entity)
+        {
+            var vendedor = _context.Vendedor.Find(entity.Id);
+
+            if (vendedor is not null)
+            {
+
+                vendedor.Nome = entity.Nome;
+                vendedor.Email = entity.Email;
+                vendedor.Telefone = entity.Endereco;
+                vendedor.DataNascimento = entity.DataNascimento;
+                vendedor.Endereco = entity.Telefone;
+                vendedor.DataContratacao = entity.DataContratacao;
+                vendedor.ComissaoPercentual = entity.ComissaoPercentual;
+                vendedor.MetaMensal = entity.MetaMensal;
+                vendedor.CriadoEm = entity.CriadoEm;
+
+                _context.Vendedor.Update(vendedor);
+                _context.SaveChanges();
+
+                return vendedor;
+            }
+
+            return null;
+        }
+
+        public VendedorEntity? DeletarDados(int id)
+        {
+            var vendedor = _context.Vendedor.Find(id);
+
+            if (vendedor is not null)
+            {
+                _context.Vendedor.Remove(vendedor);
+                _context.SaveChanges();
+
+                return vendedor;
+            }
+
+            return null;
+        }
     }
 }
